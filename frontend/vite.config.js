01c9a1ce55c00c36eb042a520c917fe3d2ddf9c8
Nikhil Muthukumar <nikeawesome132@gmail.com>
2026-07-15 17:52:30 -0500

    fixes to 3rd party

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3010,
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8010',
         changeOrigin: true,
       },
     },
